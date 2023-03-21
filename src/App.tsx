2f48d61c2b74c9e2ad6bcb921b99420cf0c5d474
Fabiana Santos Soares <faabiana.macedo@gmail.com>
2023-03-21 10:33:07 +0044

    Iniciando o projeto React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <p>Meu primeiro projeto usando React</p>
+    </div>
+  )
+}
+
+export default App;
